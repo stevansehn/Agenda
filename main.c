e00f981de464419793b4386fb05e11d6b704639e
viniciusknabben <viniciusknabben@hotmail.com>
2018-09-16 02:49:21 -0300

    Menu principal adicionado
    * main.c: loop principal criado
    * agenda.h: struct Contato corrigida
    
    Signed-off-by: viniciusknabben <viniciusknabben@hotmail.com>

--- main.c
+++ main.c
@@ -1,10 +1,54 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
+#include "agenda.h"
 
 int main ()
 {
-	system("clear");
-	printf("\t\tMy Agenda\n\n");
+	int select;
+	char input [20];
+	struct Contato ctt;
 
+	// loop
+	while(1)
+	{
+		system("clear");
+		printf("\t\tMy Agenda\n\n");
+		printf("1: Pesquisar\n");
+		printf("2: Novo Contato\n");
+
+		scanf("%i", &select);
+		switch(select)
+		{
+			case 1:
+				system("clear");
+				printf("\t\tBuscar contato:\n\n");
+				printf("Nome: ");
+				scanf("%s",input);
+				//pesquisar na agenda e rtoranr resultados
+			break;
+
+			case 2:
+				system("clear");
+				printf("\t\tAdicionar contato:\n\n");
+				
+				printf("nome: ");
+				scanf("%s",ctt.nome);
+				
+				
+				printf("telefone: ");
+				scanf("%s",ctt.telefone);
+				
+				printf("email: ");
+				scanf("%s",ctt.email);
+
+				// Adicionar contato
+			break;
+				
+
+		}
+
+	}
 	return 0;
 }
