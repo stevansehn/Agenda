7e6dc7ebac241b0dd6f9261f1fcd3e53828be03f
Vinicius Knabben <viniciusknabben@hotmail.com>
2018-09-17 22:18:24 -0300

    Prototipos de funções criados
    Signed-off-by: Vinicius Knabben <viniciusknabben@hotmail.com>

--- agenda.h
+++ agenda.h
@@ -4,10 +4,17 @@
 *
 *****************************************/
 
-struct Contato
+typedef struct Contatos
 {
 	char nome[20];
 	char telefone[13];
 	char email[20];	
-};
+} Contato;
 
+int adicionar(Contato c);
+
+int remover(Contato c);
+
+int buscar(char* s);
+
+int total();
