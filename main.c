73f27bd5f88c2b57d9eb961f8efa460004a7db12
Vinicius Knabben <viniciusknabben@hotmail.com>
2018-09-23 18:13:16 -0300

    Changed menu msg for search contact
    Signed-off-by: Vinicius Knabben <viniciusknabben@hotmail.com>

--- main.c
+++ main.c
@@ -23,7 +23,7 @@ int main ()
 		{
 			case 1:
 				system("clear");
-				printf("\t\tBuscar contato:\n\n");
+				printf("\t\tPesquisar contato:\n\n");
 				printf("Nome: ");
 				scanf("%s",input);
 				//pesquisar na agenda e rtoranr resultados
